c6972afc971f88617d8a2f00a1729dc53e051068
zb1ndev <zbindev@gmail.com>
2026-01-15 21:40:34 -0600

    Initial Commit

---
+++ hawtui.h
@@ -0,0 +1,17 @@
+#if !defined(HAWKTUI_H)
+#define HAWKTUI_H
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+    #include <stdint.h>
+    #include <string.h>
+
+    
+
+#if defined(HAWKTUI_IMPLEMENTATION)
+
+
+
+#endif // HAWKTUI_IMPLEMENTATION
+#endif // HAWKTUI_H
---
+++ main.c
@@ -0,0 +1,10 @@
+#define HAWKTUI_IMPLEMENTATION
+#include "hawtui.h"
+
+int main(void) {
+
+    printf("Hello World\n");
+
+    return 0;
+        
+}
